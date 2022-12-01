a75dcaddc884dedb406c759cf15ac7ff5fd9c02f
Lina <Linamei0616@gmail.com>
2022-12-01 00:20:28 -0600

    Edited the Alerts' Information + Added in Dictionary to retrieve Exp dates from the grocery names

---
+++ FreshFridge/FreshFridge/ExpDateFunct.swift
@@ -0,0 +1,24 @@
+//
+//  ExpDateFunct.swift
+//  FreshFridge
+//
+//  Created by 41 Go Team on 11/30/22.
+//
+
+import Foundation
+
+var ExpDates = ["Apple" : 28,
+                "Broccoli" : 10,
+                "Banana" : 7,
+                "Cabbage" : 28,
+                "Carrot" : 28,
+                "Cilantro" : 4,
+                "Corn" : 35,
+                "Cucumbers" : 7,
+                "Garlic" : 168,
+                "Lettuce" : 7,
+                "Onion" : 24,
+                "Pepper" : 14,
+                "Potato" : 84,
+                "Tomato" : 14, ]
+
---
+++ FreshFridge/FreshFridge/Views/AmountWastedScreen.swift
@@ -0,0 +1,20 @@
+//
+//  AmountWastedScreen.swift
+//  FreshFridge
+//
+//  Created by 41 Go Team on 12/1/22.
+//
+
+import SwiftUI
+
+struct AmountWastedScreen: View {
+    var body: some View {
+        Text("Pounds Wasted: 5 lbs")
+    }
+}
+
+struct AmountWastedScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        AmountWastedScreen()
+    }
+}
